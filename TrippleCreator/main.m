4c1bcfd6c34cc32ff3cde0c4f247563df1fa4a40
Denis Sancov <denis.sancov@icloud.com>
2016-04-12 16:16:21 +0300

    Initial Commit

---
+++ TrippleCreator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TrippleCreator
+//
+//  Created by Denis Sancov on 12.04.16.
+//  Copyright © 2016 University of Crete. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
